101e16950e38bacb218efe4f3bc1927b0b690ed0
Steven 'Steve' Kendall <skendall@neverware.com>
2017-06-02 14:15:35 -0400

    remove mariachi frogs for the time being

--- main.cc
+++ main.cc
@@ -9,7 +9,6 @@
 int main(int argc, char *argv[])
 {
     Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
-    Q_INIT_RESOURCE(gondarwizard);
     QApplication app(argc, argv);
     GondarWizard wizard;
     wizard.show();
